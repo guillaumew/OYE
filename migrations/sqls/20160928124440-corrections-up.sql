438638b4c4d1cdf83d5bbab2b17f4f13a7e32037
guillaumew <geeyom12@gmail.com>
2016-09-28 14:50:11 +0200

    db correction

---
+++ migrations/sqls/20160928124440-corrections-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+UPDATE Places
+SET latitude=47.214033, longitude=-1.563049
+WHERE id=6;
+
+
+UPDATE Places
+SET latitude=47.211993, longitude=-1.561813
+WHERE id=7;
